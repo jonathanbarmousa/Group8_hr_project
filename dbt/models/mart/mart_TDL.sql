a328a7a63c0d7e0a2501566da64d15bd686560f2
abdulrahmanfahmi <abdirahman3016@gmail.com>
2025-09-13 23:09:47 +0200

    mart

--- dbt/models/mart/mart_TDL.sql
+++ dbt/models/mart/mart_TDL.sql
@@ -1,15 +1,14 @@
 with 
-    fct_table as (select * from {{ref('fct_table')}}),
-    dim_employer as (select * from {{ref('dim_employer')}}) ,
-    dim_auxilliary as (select * from {{ref('dim_auxilliary_attributes')}}),
-    dim_job_details as (select * from {{ref('dim_job_details')}}),
-    dim_occupation as (select * from {{ref('dim_occupation')}})
-
+    fct_table as (select * from fct_table),
+    dim_employer as (select * from dim_employer),
+    dim_auxiliary as (select * from dim_auxiliary_attributes),
+    dim_job_details as (select * from dim_job_details),
+    dim_occupation as (select * from dim_occupation)
 
 select
-    do.occupation,
-    do.occupation_field,
-    do.occupation_group,
+    docc.occupation,
+    docc.occupation_field,
+    docc.occupation_group,
     dj.headline,
     dj.description,
     dj.employment_type,
@@ -27,10 +26,9 @@ select
     de.workplace_country,
     ft.vacancies,
     ft.application_deadline
-from
-    fct_table ft
-left join dim_employer de ON de.employer_id = ft.employer_id
-left join dim_auxilliary da ON da.auxilliary_attribute_id = ft.auxilliary_attribute_id
-left join dim_job_details dj ON dj.job_details_id = ft.job_details_id
-left join dim_occupation do ON do.occupation_id = ft.occupation_id
-where occupation_field = 'Transport, distribution, lager'
+from fct_table ft
+left join dim_employer de on de.employer_id = ft.employer_id
+left join dim_auxiliary da on da.auxiliary_attribute_id = ft.auxiliary_attribute_id
+left join dim_job_details dj on dj.job_details_id = ft.job_details_id
+left join dim_occupation docc on docc.occupation_id = ft.occupation_id
+where docc.occupation_field = 'Transport, distribution, lager'
